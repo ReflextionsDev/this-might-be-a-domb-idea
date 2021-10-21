4eae89a2bdcff3e60d7961a2996e84e001c0fbc7
ReflextionsDev <ReflextionsDev@gmail.com>
2021-10-21 14:26:08 -0400

    Complete assignment

---
+++ main.js
@@ -0,0 +1,20 @@
+const p = document.querySelector('p');
+p.style.color = 'cyan'
+
+const h = document.querySelector('h1')
+h.style.fontSize = '48px'
+
+
+p.innerHTML = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+const item13 = document.querySelector('#item-13')
+item13.style.opacity = '0.5'
+item13.innerHTML = 'I say, "Hi!"'
+
+const img = document.querySelector('img')
+img.src = 'http://www.tioxic.com/wp-content/uploads/trex_4.jpg'
+img.height = '300'
+
+const item16 = document.querySelector('#item-16')
+item16.classList.add('item')
+item16.innerHTML = "Won't get fooled again."
